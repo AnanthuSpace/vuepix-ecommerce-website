b3b6363fc9011e45960bbe597a9bb36f25a2bd1b
Ananthu Mohan <ananthumohan368@gmail.com>
2024-01-20 15:28:17 +0530

    uSer SingUp without otp

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,17 +1,139 @@
 const { User } = require("../models/userDB")
+const bcrypt = require("bcrypt")
 
+
+// Guest Account Rendering
+const renderGuest = async(req,res)=>{
+    res.render("user/userHome")
+}
 
 // Login page rendering
-
-const renderLogin = async (req,res)=>{
-          res.render("user/userLogin");
+const renderLogin = async (req, res) => {
+    try {
+        if (!req.session.user) {
+            res.render("user/userLogin");
+        } else {
+            res.redirect("/Home")
+        }
+    } catch (error) {
+        res.render("user/userHome")
+    }
 }
-const renderSignUp = async (req,res)=>{
-    res.render("user/signUp")
+
+// SignUp page Rendering
+const renderSignUp = async (req, res) => {
+    try {
+        res.render("user/signUp")
+    } catch (error) {
+        res.redirect("/signup")
+    }
+}
+
+// userHome page rendering
+const renderHome = async (req, res) => {
+    try {
+        res.render("user/userHome")
+    } catch (error) {
+        res.redirect("/login")
+    }
+}
+
+// Login and User Verification
+const userVerification = async (req, res) => {
+    const { email, password } = req.body
+
+    try {
+        const user = await User.findOne({ email })
+
+        if (!user || !(await bcrypt.compare(password, user.password))) {
+            res.render("user/userLogin")
+            console.log("Invalid User id and password");
+            return;
+        }
+
+        console.log(user.username);
+
+        req.session.user = user.username
+        res.cookie("sessionId", req.sessionId, { httpOnly: true })
+        res.render("user/userHome")
+    } catch (error) {
+        console.log("Login Error : ", error);
+    }
 }
 
 
-module.exports= {
+// Register the user details and validation
+const createUser = async (req, res) => {
+    const { username, email, phone, password, confirmPassword } = req.body
+
+    console.log(req.body);
+    const trimmedUsername = username.trim()
+
+    if (!trimmedUsername) {
+        res.render('user/signUp')
+        console.log("UserName cannot be just whitespace");
+        return
+    }
+
+    if (password !== confirmPassword) {
+        res.render("user/signUp")
+        console.log("Password missmatch");
+        return
+    }
+
+    try {
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            console.log("Existed username");
+            return res.render("user/singUp")
+        }
+
+        const hashPassword = await bcrypt.hash(password, 10)
+
+        const newUser = new User({
+            username,
+            email,
+            phone,
+            password: hashPassword
+        })
+
+        await newUser.save();
+        console.log("user Created Succcesfully");
+        res.redirect("/otp")
+    } catch (error) {
+        console.error("Error Creating User : ", error);
+    }
+}
+
+// Otp Verification
+const otpVerification = async (req,res)=>{
+    res.render("user/verify")
+}
+
+
+
+// Logout User
+const logout = async (req, res) => {
+    try {
+        req.session.destroy((err) => {
+            if (err) {
+                console.log("Logout Error");
+            }
+            console.log("Logout Successfully");
+            res.redirect("/login")
+        })
+    } catch (error) {
+        console.log("Logout Error : ", error);
+    }
+}
+
+module.exports = {
+    renderGuest,
     renderLogin,
-    renderSignUp
+    renderSignUp,
+    renderHome,
+    createUser,
+    userVerification,
+    otpVerification,
+    logout,
 }
--- routers/UserRouter.js
+++ routers/UserRouter.js
@@ -2,7 +2,14 @@ const express = require("express")
 const router = express.Router()
 const controller = require("../controllers/UserController")
 
-router.get("/", controller.renderLogin)
+
+router.get("/", controller.renderGuest)
+router.get("/login", controller.renderLogin)
 router.get("/signup", controller.renderSignUp)
+router.get("/otp", controller.otpVerification)
+router.post("/userCreate", controller.createUser);
+router.get("/Home", controller.renderHome)
+router.post("/userVerification", controller.userVerification)
+router.get("/logout", controller.logout)
 
 module.exports = router
