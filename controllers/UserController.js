640a7e3ef7989ca597930f091206c182750c8d56
Ananthu Mohan <ananthumohan368@gmail.com>
2024-01-21 00:25:37 +0530

    nodemailer is done

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,13 +1,19 @@
-const { User } = require("../models/userDB")
+const mailer = require("nodemailer")
+const { User } = require("../models/userSchema")
 const bcrypt = require("bcrypt")
 
 
 // Guest Account Rendering
+
 const renderGuest = async(req,res)=>{
+
     res.render("user/userHome")
+
 }
 
+
 // Login page rendering
+
 const renderLogin = async (req, res) => {
     try {
         if (!req.session.user) {
@@ -20,7 +26,9 @@ const renderLogin = async (req, res) => {
     }
 }
 
+
 // SignUp page Rendering
+
 const renderSignUp = async (req, res) => {
     try {
         res.render("user/signUp")
@@ -29,7 +37,24 @@ const renderSignUp = async (req, res) => {
     }
 }
 
+
+//generate otp
+
+const generateOTP = () => {
+    const otpLength = 6;
+    let otp = '';
+
+    for (let i = 0; i < otpLength; i++) {
+        const digit = Math.floor(Math.random() * 10); // Generate a random digit (0-9)
+        otp += digit.toString(); // Append the digit to the OTP string
+    }
+
+    return otp;
+};
+
+
 // userHome page rendering
+
 const renderHome = async (req, res) => {
     try {
         res.render("user/userHome")
@@ -38,8 +63,11 @@ const renderHome = async (req, res) => {
     }
 }
 
+
 // Login and User Verification
+
 const userVerification = async (req, res) => {
+
     const { email, password } = req.body
 
     try {
@@ -56,6 +84,7 @@ const userVerification = async (req, res) => {
         req.session.user = user.username
         res.cookie("sessionId", req.sessionId, { httpOnly: true })
         res.render("user/userHome")
+
     } catch (error) {
         console.log("Login Error : ", error);
     }
@@ -63,10 +92,11 @@ const userVerification = async (req, res) => {
 
 
 // Register the user details and validation
-const createUser = async (req, res) => {
-    const { username, email, phone, password, confirmPassword } = req.body
 
-    console.log(req.body);
+const createUser = async (req, res) => {
+
+    const { username, email, phone, password, confirmPassword } = req.body
+    // console.log(req.body);
     const trimmedUsername = username.trim()
 
     if (!trimmedUsername) {
@@ -89,21 +119,57 @@ const createUser = async (req, res) => {
         }
 
         const hashPassword = await bcrypt.hash(password, 10)
+        const otp =generateOTP()
 
-        const newUser = new User({
+
+        // Store user datat temporary session
+
+        req.session.tempUser = {
             username,
             email,
             phone,
-            password: hashPassword
-        })
+            password: hashPassword,
+            otp
+        }
 
-        await newUser.save();
-        console.log("user Created Succcesfully");
+
+        // Create a transporter with your email service configuration
+
+        const transporter = mailer.createTransport({
+            service: 'gmail',
+            auth: {
+              user: process.env.EMAIL_ID,
+              pass: process.env.EMAIL_PASS
+            }
+          });
+
+
+        // Set Email options
+
+        const mailOption = {
+            from: process.env.EMAIL_ID,
+            to: email,
+            subject: "OTP Verification",
+            text: `Your OTP for Verification is ${otp}`
+        }
+
+        // Sending email
+
+        transporter.sendMail(mailOption, (error,info)=>{
+
+            if(error){
+                console.error("Mailing error",error);
+            }else{
+                console.log('Email sent: ' + info.response);
+            }
+
+        })
         res.redirect("/otp")
     } catch (error) {
         console.error("Error Creating User : ", error);
     }
 }
+
 
 // Otp Verification
 const otpVerification = async (req,res)=>{
@@ -111,8 +177,37 @@ const otpVerification = async (req,res)=>{
 }
 
 
+// Handle OTP Submission
+
+const verifyOtp = async (req,res) => {
+
+    const { enteredOtp } = req.body
+    const {username, email, phone, password,otp} = req.session.tempUser
+
+    if( enteredOtp != otp){
+        console.log("Invalid OTP");
+        return res.redirect("/otp")
+    }
+    try {
+        const newUser = new User({
+            username,
+            email,
+            phone,
+            password
+        })
+
+        await newUser.save();
+        console.log("User Creeated Successfully");
+        res.redirect("/login")
+
+    } catch (error) {
+        res.redirect("/otp")
+    }
+}
+
 
 // Logout User
+
 const logout = async (req, res) => {
     try {
         req.session.destroy((err) => {
@@ -136,4 +231,5 @@ module.exports = {
     userVerification,
     otpVerification,
     logout,
+    verifyOtp
 }
