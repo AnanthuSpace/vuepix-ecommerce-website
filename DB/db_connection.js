72b2f8869138aa12cf4e4f97e7fc51fd79b38c9e
Ananthu Mohan <ananthumohan368@gmail.com>
2024-03-22 11:37:04 +0530

    Change mongo link to env

--- DB/db_connection.js
+++ DB/db_connection.js
@@ -2,8 +2,7 @@ const mongoose = require("mongoose")
 require('dotenv').config();
 
 
-const dbconnect = mongoose.connect("mongodb://ananthumohan:Namithasabu2003@ac-cijy2sg-shard-00-00.x1kojm6.mongodb.net:27017,ac-cijy2sg-shard-00-01.x1kojm6.mongodb.net:27017,ac-cijy2sg-shard-00-02.x1kojm6.mongodb.net:27017/VuePix?ssl=true&replicaSet=atlas-lzlwlo-shard-0&authSource=admin&retryWrites=true&w=majority")
-
+const dbconnect = mongoose.connect(process.env.CONNECT)
 dbconnect
     .then(() => console.log("DB Connected"))
     .catch((err) => console.error("DB Connection Error:", err.message));
